f72c3dfd6845677df0f3ba6bca17d1258ce8c904
web-altaf-1 <web.altaf.1@gmail.com>
2022-08-14 00:53:28 +0600

    all added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
-import './App.css';
-import io from 'socket.io-client'; 
-import { useState } from 'react';
-import Chat from './Chat';
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
 
-const socket= io.connect('http://localhost:4000');
+const socket = io.connect("http://localhost:3001");
 
 function App() {
-  const [username,setUsername] = useState('');
-  const [room,setRoom] = useState('');
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
-  const joinRoom = ()=>{
-    if(username !== '' && room !== ''){
-      socket.emit('join_room',room);
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
     }
-  }
+  };
 
   return (
     <div className="App">
-      <h3>Join A Chat</h3>
-      <input onChange={(event)=>{setUsername(event.target.value)}} type="text" placeholder='John...' name="" id="" />
-      <input onChange={(event)=>{setRoom(event.target.value)}} type="text" placeholder='Room Id' name="" id="" />
-      <button onClick={joinRoom}>Join A Room</button>
-      <Chat socket={socket} username={username} room={room}></Chat>
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </div>
   );
 }
